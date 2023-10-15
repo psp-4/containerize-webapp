ab289568cd9de24ff24a3e58d5338eeddd214438
psp4 <cod.psp4@gmail.com>
2023-10-15 09:23:56 +0530

    Script file fixed

--- web_files.sh
+++ web_files.sh
@@ -1,6 +1,4 @@
-#!bin/bash
-
-#Defining Variables
+# Defining Variables
 URL="https://www.tooplate.com/zip-templates/2132_clean_work.zip"
 ZIP_NAME="2132_clean_work.zip"
 FOLDER_NAME="2132_clean_work"
@@ -14,24 +12,24 @@ sudo apt update && sudo apt install unzip -y
 echo "#########################################################"
 echo "Creating Folder"
 echo "#########################################################"
-mkdir $PROJECT_FOLDER
-cd $PROJECT_FOLDER
+mkdir "$PROJECT_FOLDER"
+cd "$PROJECT_FOLDER"
 
 echo "#########################################################"
-echo "Fetching webfiles from tooplate server"
+echo "Fetching web files from tooplate server"
 echo "#########################################################"
-wget $URL
+wget "$URL"
 
 echo "#########################################################"
 echo "Unzipping Fetched files & Removing the zip file"
 echo "#########################################################"
-unzip $ZIP_NAME
-rm $ZIP_NAME
-cd $FOLDER_NAME
+unzip "$ZIP_NAME"
+rm "$ZIP_NAME"
+cd "$FOLDER_NAME"
 
 echo "#########################################################"
 echo "Creating tarball & Removing temporary files"
 echo "#########################################################"
 tar czvf clean.tar.gz *
 mv clean.tar.gz ../
-rm -rf $FOLDER_NAME
+rm -rf "$FOLDER_NAME"
